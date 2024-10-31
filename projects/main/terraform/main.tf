0a1aa99ce8138eb22bfe4d8b3b51424238f33c2f
Yunior Alayo <yunior.alayo@gmail.com>
2024-10-31 18:30:11 +0100

    Removed non needed aws provider.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -1,12 +1,5 @@
 # provider config
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.7.0"
-    }
-  }
-
   cloud {
     organization = "rondon-sarnik"
 
