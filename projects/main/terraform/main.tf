2e9e61bf132c79451750973a86fc2b0f87b344a4
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-15 18:40:13 +0100

    Adding variable again.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -20,7 +20,9 @@ variable "cloudflare_api_token" {
   default = ""
 }
 
-provider "cloudflare" {}
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
 
 resource "cloudflare_workers_script" "score_backend" {
   account_id = "f037e56e89293a057740de681ac9abbe"
